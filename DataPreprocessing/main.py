aa4a5723f5c1d285d825a5905fbdb4c0e29a4aaf
Gonçalo <117322115+GoncaloSantosCosta@users.noreply.github.com>
2026-01-22 13:38:24 +0100

    Update main.py

--- DataPreprocessing/main.py
+++ DataPreprocessing/main.py
@@ -1,1 +1,15 @@
+import utils
 
+edf_dir = "DATADIRECTORY"      # Replace with your actual data directory path
+model_path = "MODELDIRECTORY"  # Replace with the trained model's path
+
+# Set the start and end timestamps for the segment you want to preprocess
+start = "segment start timestamp"
+end = "segment end timestamp"
+
+raw_data, preprocessed_data = utils.preprocess_data(
+    edf_dir,
+    model_path,
+    start,
+    end
+)
